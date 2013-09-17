784ac3a4dd7cfd4962c8db702a4acb50bf5a9836
mffloe13 <matsf10@hotmail.com>
2013-09-17 12:06:17 +0200

    Create lab2.js

---
+++ lab2.js
@@ -0,0 +1,8 @@
+var element_unordered_list = document.getElementById("lab2");alert(element_unordered_list);element_unordered_list.style.color = "green";
+function printToOutputBox(selectobj){
+ //var  = document.getElementById("lab2");
+ //var e_outputbox = document.getElementById("outputbox");
+ 
+ document.getElementById("lab2").innerHTML="Beklager, denne siden er under konstruksjon";
+ e_outputbox.innerHTML = "Beklager, siden er under konstruksjon" + e_proglangs.options[selectobj.selectedIndex].text;
+}
